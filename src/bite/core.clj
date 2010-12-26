856e30640b781493543cd9467d2bfbf67f47fe58
Ethan <ethan@sadin.(none)>
2010-12-26 13:45:22 -0500

    basic routing

--- src/bite/core.clj
+++ src/bite/core.clj
@@ -2,24 +2,31 @@
   (:use ring.adapter.jetty
         ring.middleware.params))
 
+(defn bad-uri [req]
+  "Default handler for unmatched requests"
+  {:status 404
+   :headers {"Content-Type" "text/html"}
+   :body "Bad Request"
+   })
+
 (defn route [routes]
+  "Build a function which routes a request to the correct handler."
   (fn [req]
     (let [uri (:uri req)
           r-keys (map re-pattern (keys routes))
-          debug (println r-keys)
           matches (filter #(re-seq %1 uri) r-keys)
-          debug (println matches)
-          m-funs (map #(get routes (str %1)) matches)
-          debug (println m-funs)]
-      (if m-funs (apply (first m-funs) [req]) "404: No Matching Response"))))
+          m-funs (map #(get routes (str %1)) matches)]
+      (apply (first (concat m-funs [bad-uri])) [req]))))
 
 
-(defn idem [req] (merge req {:special "true"}))
+;; Testing 
+
+(defn idem-p [req] (merge req {:special "true"}))
+(defn idem [req] req))
 
 (def app (route
-          {"/home" idem
-           "/index" idem }
-          ))
+          {"/user/.*" idem-p
+           "/index" idem }))
 
 
       
