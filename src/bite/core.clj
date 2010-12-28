3328aab78568e2d597f8ad7827ad2d5cc1d7a8d8
Ethan <ethan@sadin.(none)>
2010-12-28 15:15:42 -0500

    middleware

--- src/bite/core.clj
+++ src/bite/core.clj
@@ -2,12 +2,24 @@
   (:use ring.adapter.jetty
         ring.middleware.params))
 
+;; helper routes
+
 (defn bad-uri [req]
   "Default handler for unmatched requests"
   {:status 404
    :headers {"Content-Type" "text/html"}
    :body "Bad Request"
    })
+
+;; generic helper functions
+
+(defn compose-list [funcs arg]
+     (if (not (empty? funcs))
+       (let [nxt-f (first funcs)
+             rst (drop 1 funcs)
+             next (apply nxt-f [arg])]
+         (recur rst next))
+       arg))
 
 (defn route [routes]
   "Build a function which routes a request to the correct handler."
@@ -18,13 +30,15 @@
   (fn [req]
     (let [uri (:uri req)
           req-m (:request-method req)
+          mddl (:middleware routes)
           r-keys (map re-pattern (keys routes))
           ;; match routes against the request uri
           matches (filter #(re-seq %1 uri) r-keys)
           ;; match against request-method
           m-funs-h (map #(get routes (str %1)) matches)
-          m-funs (map #(get %1 req-m) m-funs-h)]
-      (apply (first (concat m-funs [bad-uri])) [req]))))
+          m-funs (map #(get %1 req-m) m-funs-h)
+          res-fun (first (concat m-funs [bad-uri]))]
+      (apply (compose-list mddl res-fun) [req]))))
 
 
 ;; Testing 
@@ -33,10 +47,14 @@
 (defn idem [req] req))
 
 (def app (route
-          {"/user/.*" {:get idem-p
-                       :post idem}
-           "/index" {:get idem-p
-                     :post idem}}))
+          { "/user/.*"
+              {:get idem-p
+               :post idem}
+            "/index"
+              {:get idem-p
+               :post idem}
+            :middleware
+              [wrap-params] }))
 
 
       
